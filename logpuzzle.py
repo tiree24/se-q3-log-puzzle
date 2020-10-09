433f47ee7a1572df1bea057f81690b65ca84beaa
Tiree Jackson <tiree_jackson@yahoo.com>
2020-10-09 16:42:58 -0400

    forgot to make a new branch

--- logpuzzle.py
+++ logpuzzle.py
@@ -14,7 +14,7 @@ HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;
 rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
 
-__author__ = "Tiree I got help from Mike the coach"
+__author__ = "Tiree I got help from Mike the coach."
 
 import os
 import re
